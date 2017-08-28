34309075a58a8373ee28ba2349ac34de634d13b1
Rafael Gumieri <rafael.gumieri@tecnospeed.com.br>
2017-08-28 19:26:48 -0300

    Better control of the flux to create PDF
    - Add .tool-versions (Node.js 8.4.0)
    
    API:
    - A general refactory
    
    Docker:
    - No more using root user
    - .dockerignore ignoring itself, node_modules and .tool-versions

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-const url = require("url")
-const http = require("http")
-const winston = require("winston")
-const puppeteer = require("puppeteer")
 const formidable = require("formidable")
+const browser = require("./lib/browser")
+const winston = require("winston")
+const http = require("http")
+const url = require("url")
 
-function printUri(uri) {
-  return new Promise((resolve, reject) => {
-    if (!uri) return reject("no url/html provided")
-    winston.info(`requested: ${uri}`)
-
-    let args = ["--no-sandbox", "--disable-setuid-sandbox", "--disable-gpu"]
-
-    puppeteer
-      .launch({ args })
-      .then(async browser => {
-        let page = await browser.newPage()
-
-        await page.goto(uri, { waitUntil: "networkidle" })
-
-        resolve(
-          await page.pdf({
-            format: "A4"
-          })
-        )
-
-        browser.close()
-      })
-      .catch(reject)
-  })
-}
-
-function pickUri(request) {
+function requestToUri(request) {
   return new Promise((resolve, reject) => {
     let query = url.parse(request.url, true).query
     if (query.url) return resolve(query.url)
@@ -50,23 +24,25 @@ function pickUri(request) {
 }
 
 function handler(request, response) {
-  if (request.url === "/favicon.ico") {
-    response.writeHead(200, { "Content-Type": "image/x-icon" })
-    response.end()
-    return
-  }
-
-  pickUri(request)
-    .then(printUri)
-    .then(pdf => {
-      response.writeHead(200, { "Content-Type": "application/pdf" })
-      response.end(pdf)
-    })
-    .catch(err => {
-      response.writeHead(400, { "Content-Type": "text/plain" })
+  switch (request.url) {
+    case "/favicon.ico":
+      response.writeHead(200, { "Content-Type": "image/x-icon" })
       response.end()
-      winston.error(err)
-    })
+      break
+
+    default:
+      requestToUri(request)
+        .then(browser.uriToPdf)
+        .then(pdf => {
+          response.writeHead(200, { "Content-Type": "application/pdf" })
+          response.end(pdf)
+        })
+        .catch(err => {
+          response.writeHead(400, { "Content-Type": "text/plain" })
+          response.end()
+          winston.error(err)
+        })
+  }
 }
 
 http.createServer(handler).listen(process.env.PORT)
